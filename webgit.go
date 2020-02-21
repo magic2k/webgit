ea7f502c1a948b0c4608168f9fc9c62f01ea88c1
Vladimir Kolosov <magic2ktech@gmail.com>
2020-02-21 16:32:49 +0300

    added basic auth, external config, fixed systemd script

--- webgit.go
+++ webgit.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"bufio"
+	"crypto/subtle"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 )
 
+func readConfig(confName string) map[string]string {
+	m := make(map[string]string)
+
+	confFile, err := ioutil.ReadFile(confName)
+	if err != nil {
+		log.Fatal(err)
+		panic("Conf file cannot be read")
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(confFile)))
+	for scanner.Scan() {
+		prop := strings.Split(scanner.Text(), " ")
+		m[prop[0]] = prop[1]
+	}
+	fmt.Println(m)
+	return m
+}
+
 func main() {
-	port := ":8080"
-	gitdir := "/var/www/html/CustomCode"
+	configMap := readConfig("properties.conf")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	port := configMap["port"]
+	gitdir := configMap["gitdir"]
+	username := configMap["username"]
+	password := configMap["password"]
+	msg := "Please enter your username and password for this site"
+
+	handlerRoot := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, this is git utility\nPossible urls:\n/status\n/log\n/pull\n/rollback\n/branch?<branch_name>")
-	})
+	}
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+	handlerStatus := func(w http.ResponseWriter, r *http.Request) {
 		cmd := exec.Command("git", "-C", gitdir, "status")
 		output, err := cmd.Output()
 		//	log.Printf("Output: %s", output)
@@ -24,9 +51,9 @@ func main() {
 		if err != nil {
 			log.Printf("Command finished with error: %v", err)
 		}
-	})
+	}
 
-	http.HandleFunc("/pull", func(w http.ResponseWriter, r *http.Request) {
+	handlerPull := func(w http.ResponseWriter, r *http.Request) {
 		cmd := exec.Command("git", "-C", gitdir, "pull")
 		output, err := cmd.Output()
 		fmt.Fprintf(w, "Done\n, %s", output)
@@ -34,9 +61,9 @@ func main() {
 		if err != nil {
 			log.Printf("Command finished with error: %v", err)
 		}
-	})
+	}
 
-	http.HandleFunc("/rollback", func(w http.ResponseWriter, r *http.Request) {
+	handlerRollback := func(w http.ResponseWriter, r *http.Request) {
 		cmd := exec.Command("git", "-C", gitdir, "reset", "--hard", "HEAD^")
 		output, err := cmd.Output()
 		fmt.Fprintf(w, "Done\n, %s", output)
@@ -44,9 +71,9 @@ func main() {
 		if err != nil {
 			log.Printf("Command finished with error: %v", err)
 		}
-	})
+	}
 
-	http.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+	handlerLog := func(w http.ResponseWriter, r *http.Request) {
 		cmd := exec.Command("git", "-C", gitdir, "log", "--oneline")
 		output, err := cmd.Output()
 		fmt.Fprintf(w, "Done\n, %s", output)
@@ -54,9 +81,9 @@ func main() {
 		if err != nil {
 			log.Printf("Command finished with error: %v", err)
 		}
-	})
+	}
 
-	http.HandleFunc("/branch", func(w http.ResponseWriter, r *http.Request) {
+	handlerBranch := func(w http.ResponseWriter, r *http.Request) {
 		branch := r.URL.RawQuery
 		cmd := exec.Command("git", "-C", gitdir, "checkout", branch)
 		output, err := cmd.Output()
@@ -65,7 +92,30 @@ func main() {
 		if err != nil {
 			log.Printf("Command finished with error: %v", err)
 		}
-	})
+	}
+
+	http.HandleFunc("/", basicAuth(handlerRoot, username, password, msg))
+	http.HandleFunc("/status", basicAuth(handlerStatus, username, password, msg))
+	http.HandleFunc("/pull", basicAuth(handlerPull, username, password, msg))
+	http.HandleFunc("/rollback", basicAuth(handlerRollback, username, password, msg))
+	http.HandleFunc("/log", basicAuth(handlerLog, username, password, msg))
+	http.HandleFunc("/branch", basicAuth(handlerBranch, username, password, msg))
 
 	http.ListenAndServe(port, nil)
+}
+
+func basicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		user, pass, ok := r.BasicAuth()
+
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorised.\n"))
+			return
+		}
+
+		handler(w, r)
+	}
 }
